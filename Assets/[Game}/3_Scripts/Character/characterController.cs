c897939bb6c8f0516f7bba8cb3f3fa67ae6741f0
Berk Kaya <berkkaya0304@hotmail.com>
2023-06-21 12:05:05 +0300

    ..

--- Assets/[Game}/3_Scripts/Character/characterController.cs
+++ Assets/[Game}/3_Scripts/Character/characterController.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class characterController : MonoBehaviour
 {
     public float speed = 0.0f;
-    public float jump = 0.0f;
+    [SerializeField] private float jumpPower = 1.0f;
     private Rigidbody2D r2d;
     private Animator _animator;
-    
+    private Vector3 charPos;
+    [SerializeField] private GameObject _camera;
+    private SpriteRenderer _spriteRenderer;
+    private bool isGrounded;
+
+
     void Start()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         r2d = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        charPos = transform.position;
+        
     }
 
     // Update is called once per frame
@@ -21,16 +29,31 @@ public class characterController : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             speed = 1.0f;
+            _spriteRenderer.flipX = false;
+            charPos = new Vector3(charPos.x + (speed * Time.deltaTime), charPos.y);
+            transform.position = charPos;
+            _animator.SetFloat("speed", speed);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             speed = -1.0f;
+            _spriteRenderer.flipX = true;
+            charPos = new Vector3(charPos.x + (speed * Time.deltaTime), charPos.y);
+            transform.position = charPos;
+            _animator.SetFloat("speed", speed);
         }
         else
         {
             speed = 0.0f;
         }
-        _animator.SetFloat("speed",speed);
-        r2d.velocity = new Vector2(speed, 0.0f);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x, jumpPower*0.1f);
+        }
+
+        
+        
     }
+ 
 }
