3a771df04a10abc23f3cb3439ec983dcd22ad503
MzK5547 <mehmet_55_1361@hotmail.com>
2023-07-16 21:08:36 +0300

    Düzenleme
    Bir çok hata düzeltmesi vs.

--- Assets/[Game}/3_Scripts/Character/characterController.cs
+++ Assets/[Game}/3_Scripts/Character/characterController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class characterController : MonoBehaviour
 {
 
     [SerializeField] private float jumpPower = 1.0f;
